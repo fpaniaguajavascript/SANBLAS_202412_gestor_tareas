45e19aaee939919ccc62029739ef0e92f5c02032
fpaniaguajavascript <fernando.paniagua.javascript@gmail.com>
2025-01-13 13:58:45 +0100

    Creado el componente Ficha Tarea

---
+++ src/app/components/ficha-tarea/ficha-tarea.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ficha-tarea',
+  standalone: true,
+  imports: [],
+  templateUrl: './ficha-tarea.component.html',
+  styleUrl: './ficha-tarea.component.css'
+})
+export class FichaTareaComponent {
+
+}
